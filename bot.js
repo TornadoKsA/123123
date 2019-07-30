0dd5543e1315c413c9d921f60fb2b7e49c6ab7b0
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-07-30 12:09:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('ready', () => {
 
   console.log(`Logged in as ${client.user.tag}!`);
 
-client.user.setGame(`System Fox`,"http://twitch.tv/S-F")
+client.user.setGame(`FoX Security`,"http://twitch.tv/S-F")
 
 client.user.setStatus("dnd")
 
