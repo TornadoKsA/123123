5f8e78a816bf3416fe9cb8f428bfed4ba5bd605f
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-07-31 14:10:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -430,4 +430,24 @@ hours = 12;
   }})
 
 
+lient.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'setRainbow')) {//to create the rainbow role
+	  let role = message.guild.roles.find('name', 'Rainbow bot.')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow bot.",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
+
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+
+
 client.login(process.env.BOT_TOKEN);
