49489e60e3e1c5d0674c2c3122e2e76941d6f6e7
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-07-31 14:02:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -334,7 +334,7 @@ client.on('message', message => {
            if (!message.channel.guild) return;
 
     let room = message.content.split(" ").slice(1);
-    let findroom = message.guild.channels.find('sug', `${room}`)
+    let findroom = message.guild.channels.find('staff', `${room}`)
     if(message.content.startsWith(prefix + "setSug")) {
         if(!message.channel.guild) return;
         if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
