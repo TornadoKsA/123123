5f7a2948d48ccbf8ef8f2771f90f9a361c2e95d6
TornadoKsA <52514451+TornadoKsA@users.noreply.github.com>
2019-08-13 13:32:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,38 @@
 const Discord = require('discord.js');
-const fs = require('fs');
-const client = new Discord.Client();
+const moment = require("moment");  
+const fs = require("fs");      
+const dateFormat = require('dateformat');
+const client = new Discord.Client(); 
+const Canvas = require("canvas"); 
+const prefix = "!"
+const id = JSON.parse(fs.readFileSync("./id/rank.json", "utf8"));
 
-client.on('ready', () => {
-  console.log(`Welcome Bro ${client.user.tag}!`);
-});
-streaming
+let banse = new Set();
+client.on('guildBanAdd', function(guild) {
+  guild.fetchAuditLogs().then(logs => {
+    const ser = logs.entries.first().executor;
+    if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
+      bans: 2
+    }
+    let boner = bane[ser.id+guild.id]
+banse.add(ser.id)
+boner.bans = Math.floor(boner.bans+1)
 
-const prefix = '!'
+
+setTimeout(() => {
+  boner.bans = 2
+  banse.delete(ser.id)
+},8000)
+
+if(boner.bans > 2) {
+  let roles = guild.members.get(ser.id).roles.array()
+guild.members.get(ser.id).removeRoles(roles)
+}
+
+    })
+    fs.writeFile('./alpha.json', JSON.stringify(bane), (err) => {
+if (err) console.error(err);
+})
 
 
 client.login(process.env.BOT_TOKEN);
